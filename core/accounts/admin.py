d43af87e5fca4cb92e3fa43e265001f2a647b5c1
sepehr-sanaei <sepehrsanaeiazad@gmail.com>
2024-11-26 19:28:45 +0330

    admin registration
    added User and Profile models to admin page

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,71 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    """
+    Custom admin panel for user management with add and change forms plus password
+    """
+
+    model = User
+    list_display = ("id", "email", "is_superuser", "is_active", "is_verified")
+    list_filter = ("email", "is_superuser", "is_active", "is_verified")
+    searching_fields = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        (
+            "Authentication",
+            {
+                "fields": ("email", "password"),
+            },
+        ),
+        (
+            "permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "is_verified",
+                ),
+            },
+        ),
+        (
+            "group permissions",
+            {
+                "fields": ("groups", "user_permissions", "type"),
+            },
+        ),
+        (
+            "important date",
+            {
+                "fields": ("last_login",),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "is_verified",
+                    "type",
+                ),
+            },
+        ),
+    )
+
+class CustomProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "first_name", "last_name", "phone_number")
+    list_filter = ("user",)
+
+admin.site.register(Profile, CustomProfileAdmin)
+admin.site.register(User, CustomUserAdmin)
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -84,4 +84,4 @@ class Profile(models.Model):
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created and instance.type == UserType.customer.value:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(user=instance, pk=instance.id)
