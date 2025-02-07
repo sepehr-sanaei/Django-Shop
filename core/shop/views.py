dedfa8a9a6cf95a83ce783e0e2848880ac4c5640
sepehr-sanaei <sepehrsanaeiazad@gmail.com>
2025-02-07 16:40:53 +0330

    product list view Done :)

--- core/shop/views.py
+++ core/shop/views.py
@@ -44,10 +44,23 @@ class ShopProductGridView(ListView):
 
 class ShopProductListView(ListView):
     template_name = 'shop/product-list.html' 
-    paginate_by = 9
+    paginate_by = 5
     
     def get_queryset(self):
         queryset = ProductModel.objects.filter(status=ProductStatusType.publish.value)
+        if search_q:=self.request.GET.get('q'):
+            queryset = queryset.filter(title__icontains=search_q)  
+        if category_id:=self.request.GET.get('category_id'):
+            queryset = queryset.filter(category__id=category_id)
+        if min_price:=self.request.GET.get('min_price'):
+            queryset = queryset.filter(price__gte=min_price)
+        if max_price:=self.request.GET.get('max_price'):
+            queryset = queryset.filter(price__lte=max_price)
+        if max_price:=self.request.GET.get('max_price'):
+            queryset = queryset.filter(price__lte=max_price)
+        if order_by:=self.request.GET.get('order_by'):
+            queryset = queryset.order_by(order_by)
+            
         return queryset
     
     def get_paginate_by(self, queryset):
